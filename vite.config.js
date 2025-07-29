007d34e41e086e4c8ca944a1af7ac98ccb32f105
Valentical <sisanekbestvideosonyt@gmail.com>
2025-07-29 18:11:46 +0000

    commit 2

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+      host: true,
+      allowedHosts: ['img.osh.gay'],
+      port: 5173,
+      cors: true
+    }
 });
