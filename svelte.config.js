c70a686c88e925b9a5aca0cbc8b163a837567de2
smanasek <66624699+Valentical@users.noreply.github.com>
2025-07-29 21:33:47 +0200

    commit 4

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: preprocess(),
     kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            precompress: false,
-            strict: true,
-        }),
-        prerender: {
-            origin: "https://img.osh.gay",
-        }
-    },
+        adapter: adapter(),
+    }
 };
 
 export default config;
